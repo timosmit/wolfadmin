e8a226cb53b6ddbeed6d9ebfd3e8901d143bf511
Timo Smit <t.smitje@hotmail.com>
2016-08-26 10:19:01 +0200

    Updated version

--- luascripts/main.lua
+++ luascripts/main.lua
@@ -41,8 +41,8 @@ local voting = require "luascripts.wolfadmin.game.voting"
 local stats = require "luascripts.wolfadmin.players.stats"
 local greetings = require "luascripts.wolfadmin.players.greetings"
 
-local version = "1.0.2"
-local release = "16 February 2016"
+local version = "1.1.0-beta"
+local release = "26 August 2016"
 
 local basepath = nil
 
