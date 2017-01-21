timosmit/wolfadmin

commit 9012d54da4aad7d6d5e62c101ee4e1b844699072

Misc auth improvements (refs #69, #75)
* renamed methods to camelCase
* renamed methods to be more precise
* auth.isPlayerAllowed now returns boolean values

luamods/wolfadmin/auth/acl.lua
@@ -48,14 +48,10 @@ function acl.clearCache()
     data = {}
 end
 
-function acl.isallowed(clientId, permission)
+function acl.isPlayerAllowed(clientId, permission)
     local level = acl.getlevel(clientId)
 
-    if data[level] ~= nil and tables.contains(data[level], permission) then
-        return 1
-    end
-
-    return 0
+    return data[level] ~= nil and tables.contains(data[level], permission)
 end
 
 function acl.getLevels()
@@ -118,13 +114,13 @@ function acl.removeLevelRoles(levelId)
     data[levelId] = {}
 end
 
-function acl.getlevel(clientId)
+function acl.getPlayerLevel(clientId)
     local player = db.getplayer(players.getGUID(clientId))
 
     return player["level_id"]
 end
 
-function acl.getlevelname(levelId)
+function acl.getLevelName(levelId)
     local level = db.getLevel(levelId)
 
     return level["name"]

luamods/wolfadmin/auth/auth.lua
@@ -112,7 +112,7 @@ auth.PERM_IMMUNE = "immune"
 -- as this module serves as a wrapper/super class, we load the selected database
 -- system in this function. might have to think of a better way to implement
 -- this, but it will suffice.
-function auth.oninit()
+function auth.onGameInit()
     if settings.get("g_standalone") == 1 then
         srv = require (wolfa_getLuaPath()..".auth.acl")
     else
@@ -125,6 +125,6 @@ function auth.oninit()
     
     setmetatable(auth, {__index = srv})
 end
-events.handle("onGameInit", auth.oninit)
+events.handle("onGameInit", auth.onGameInit)
 
 return auth

luamods/wolfadmin/auth/shrubbot.lua
@@ -111,11 +111,11 @@ local flags = {
     [auth.PERM_IMMUNE] = "!",
 }
 
-function shrubbot.isallowed(clientId, permission)
-    return et.G_shrubbot_permission(clientId, flags[permission])
+function shrubbot.isPlayerAllowed(clientId, permission)
+    return et.G_shrubbot_permission(clientId, flags[permission]) == 1
 end
 
-function shrubbot.getlevel(clientId)
+function shrubbot.getPlayerLevel(clientId)
     return et.G_shrubbot_level(clientId)
 end
 

luamods/wolfadmin/commands/admin/admintest.lua
@@ -24,8 +24,8 @@ local players = require (wolfa_getLuaPath()..".players.players")
 local settings = require (wolfa_getLuaPath()..".util.settings")
 
 function commandAdminTest(clientId, cmdArguments)
-    local level = auth.getlevel(clientId)
-    local levelName = auth.getlevelname(level)
+    local level = auth.getPlayerLevel(clientId)
+    local levelName = auth.getLevelName(level)
 
     et.trap_SendConsoleCommand(et.EXEC_APPEND, "cchat -1 \"^dadmintest: ^7"..players.getName(clientId).." ^9is a level "..level.." user (^7"..levelName.."^9).\";")
 

luamods/wolfadmin/commands/admin/ban.lua
@@ -55,17 +55,17 @@ function commandBan(clientId, cmdArguments)
         duration = util.getTimeFromString(cmdArguments[2])
     elseif cmdArguments[2] then
         reason = table.concat(cmdArguments, " ", 2)
-    elseif auth.isallowed(clientId, "8") ~= 1 then
+    elseif not auth.isPlayerAllowed(clientId, "8") then
         et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dban usage: "..commands.getadmin("ban")["syntax"].."\";")
         
         return true
     end
 
-    if auth.isallowed(cmdClient, "!") == 1 then
+    if auth.isPlayerAllowed(cmdClient, "!") then
         et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dban: ^7"..et.gentity_get(cmdClient, "pers.netname").." ^9is immune to this command.\";")
 
         return true
-    elseif auth.getlevel(cmdClient) > auth.getlevel(clientId) then
+    elseif auth.getPlayerLevel(cmdClient) > auth.getPlayerLevel(clientId) then
         et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dban: ^9sorry, but your intended victim has a higher admin level than you do.\";")
 
         return true

luamods/wolfadmin/commands/admin/gib.lua
@@ -44,11 +44,11 @@ function commandGib(clientId, cmdArguments)
         return true
     end
 
-    if auth.isallowed(cmdClient, "!") == 1 then
+    if auth.isPlayerAllowed(cmdClient, "!") then
         et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dgib: ^7"..et.gentity_get(cmdClient, "pers.netname").." ^9is immune to this command.\";")
 
         return true
-    elseif auth.getlevel(cmdClient) > auth.getlevel(clientId) then
+    elseif auth.getPlayerLevel(cmdClient) > auth.getPlayerLevel(clientId) then
         et.trap_SendConsoleCommand(et.EXEC_APPEND, "csay "..clientId.." \"^dgib: ^9sorry, but your intended victim has a higher admin level than you do.\";")
 
         return true

luamods/wolfadmin/commands/admin/help.lua
@@ -26,7 +26,7 @@ function commandHelp(clientId, cmdArguments)
         local availableCommands = {}
         
         for command, data in pairs(cmds) do
-            if data["function"] and data["flag"] and auth.isallowed(clientId, data["flag"]) == 1 and (not data["hidden"] or (type(data["hidden"]) == "function" and not data["hidden"]())) then
+            if data["function"] and data["flag"] and auth.isPlayerAllowed(clientId, data["flag"]) and (not data["hidden"] or (type(data["hidden"]) == "function" and not data["hidden"]())) then
                 table.insert(availableCommands, command)
             end
         end

luamods/wolfadmin/commands/admin/incognito.lua
@@ -40,7 +40,7 @@ function commandIncognito(clientId, cmdArguments)
         -- et.G_Print(string.format("%s %s %d %s\n", adminName, adminGUID, adminLevel, adminFlags))
         
         if players.getGUID(clientId) == adminGUID then
-            if auth.isallowed(clientId, "@") ~= 1 then
+            if not auth.isPlayerAllowed(clientId, "@") then
                 adminFlags = adminFlags.."+@"
                 
                 et.trap_SendConsoleCommand(et.EXEC_APPEND, "cchat "..clientId.." \"^dincognito: ^9you are now playing incognito.\";")

commit e36d70aefa51a08bc419abc04a46044c41085a15

Fixed minor rename issue caused by 9012d54da4aad7d6d5e62c101ee4e1b844699072 (refs #69, #75)

luamods/wolfadmin/auth/acl.lua
@@ -49,7 +49,7 @@ function acl.clearCache()
 end
 
 function acl.isPlayerAllowed(clientId, permission)
-    local level = acl.getlevel(clientId)
+    local level = acl.getPlayerLevel(clientId)
 
     return data[level] ~= nil and tables.contains(data[level], permission)
 end