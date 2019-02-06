a37ec76d8c5a4571819227d9daba4594e56b6d69
Timo Smit <contact@timosmit.com>
2019-02-06 12:35:12 +0100

    Added missing module (refs #87)

--- luascripts/wolfadmin/admin/censor.lua
+++ luascripts/wolfadmin/admin/censor.lua
@@ -15,6 +15,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+local auth = wolfa_requireModule("auth.auth")
+
 local admin = wolfa_requireModule("admin.admin")
 local mutes = wolfa_requireModule("admin.mutes")
 local history = wolfa_requireModule("admin.history")
